b21b88fe6f860373c0d6b7e92df595aea712a6db
Maximilian Rohland <2794686+rohlandm@users.noreply.github.com>
2025-11-13 19:13:59 +0100

    test skeletons

--- src/aoc/day1solver.rs
+++ src/aoc/day1solver.rs
@@ -3,3 +3,17 @@ use crate::aoc::daysolver::DaySolver;
 pub struct Day1Solver;
 
 impl DaySolver for Day1Solver {}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_solve_part1() {
+        assert!(true)
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        assert!(true)
+    }
+}
--- src/aoc/daysolver.rs
+++ src/aoc/daysolver.rs
@@ -1,8 +1,8 @@
 pub trait DaySolver {
-    fn solve_part1(&self, input: &Vec<String>) -> Result<i64, &'static str> {
+    fn solve_part1(&self, _input: &Vec<String>) -> Result<i64, &'static str> {
         Err("😱 Part 1 not yet implemented!")
     }
-    fn solve_part2(&self, input: &Vec<String>) -> Result<i64, &'static str> {
+    fn solve_part2(&self, _input: &Vec<String>) -> Result<i64, &'static str> {
         Err("😱 Part 2 not yet implemented!")
     }
 }
