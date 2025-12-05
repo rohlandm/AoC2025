e6c69777d265f6bbd9098123a2bbce583041d7c8
Max Rohland <2794686+rohlandm@users.noreply.github.com>
2025-12-05 18:51:33 +0100

    Day4 (#17)
    * part 1
    
    * part 2
    
    * day 5 prep

---
+++ src/solutions/day5.rs
@@ -0,0 +1,19 @@
+use crate::aoc::DaySolver;
+
+pub struct Solver;
+
+impl DaySolver for Solver {}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_solve_part1() {
+        assert!(true)
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        assert!(true)
+    }
+}
