75ff01bdefee94d3dd64f3e498f2a76fb437cd0d
Maximilian Rohland <2794686+rohlandm@users.noreply.github.com>
2025-12-05 18:50:32 +0100

    day 5 prep

--- src/aoc.rs
+++ src/aoc.rs
@@ -46,7 +46,8 @@ impl TryFrom<u8> for Day {
             2 => Box::new(day2::Solver),
             3 => Box::new(day3::Solver),
             4 => Box::new(day4::Solver),
-            5..=12 => bail!("day {value} not yet implemented"),
+            5 => Box::new(day5::Solver),
+            6..=12 => bail!("day {value} not yet implemented"),
             _ => bail!("🚨 Day argument should be between 1 and 12"),
         };
         Ok(Day { day, solver })
